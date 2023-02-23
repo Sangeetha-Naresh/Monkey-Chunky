8e3a12f9f3937b64474726d1c2bda333d5c40c3b
Sangeetha-Naresh <sangeetha.nareshm@gmail.com>
2023-02-23 13:47:22 +0530

    changed border radius

--- App.js
+++ App.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius:50,
+    borderRadius:40,
   },
   goButton: {
     width: '50%',
